2af76646e5df2b01454a13cecdfa3d5e9b9f6a61
浅見 憲司 <azami@senken-kenji-no-MacBook.local>
2012-10-11 14:19:43 +0900

    Initial Commit

---
+++ RebtApplication/RebtApplicationAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  RebtApplicationAppDelegate.h
+//  RebtApplication
+//
+//  Created by 浅見 憲司 on 2012/10/11.
+//  Copyright (c) 2012年 Material-Tokorozawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RebtApplicationAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ RebtApplication/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RebtApplication
+//
+//  Created by 浅見 憲司 on 2012/10/11.
+//  Copyright (c) 2012年 Material-Tokorozawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RebtApplicationAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RebtApplicationAppDelegate class]));
+    }
+}
